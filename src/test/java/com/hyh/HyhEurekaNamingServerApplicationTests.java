6ed42f366d66660dff1295d08201486ca80d4981
Aleksandr.Kulakov <oleksandr.kulakov@nure.ua>
2020-04-03 20:59:15 +0300

    Config commit

---
+++ src/test/java/com/hyh/HyhEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyhEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
